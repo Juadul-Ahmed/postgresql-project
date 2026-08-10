83f91a6e330b28b50404d194ab911995c7fc8554
juadul <parallaxhub@gmail.com>
2026-08-10 19:47:42 +0800

    Downgrade TypeScript to 5.5.4 for Vercel compatibility

--- dist/lib/prisma.js
+++ dist/lib/prisma.js
@@ -1,11 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
-const prisma = global.prisma || new client_1.PrismaClient();
-if (process.env.NODE_ENV !== "production") {
-    global.prisma = prisma;
-}
-prisma.$connect().catch((err) => {
-    console.error("Prisma connection error:", err);
-});
+const prisma = new client_1.PrismaClient();
 exports.default = prisma;
